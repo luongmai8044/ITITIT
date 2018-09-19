ceb189d86ec37500de0304f6aa57c1d4a0cd42e1
Luong Mai <mwd-luong.mai@mowede.com>
2018-09-19 14:29:05 +0700

    Initial commit

---
+++ app/src/main/java/mowede/framework/ititit/data/network/ApiHelper.kt
@@ -0,0 +1,15 @@
+package mowede.framework.ititit.data.network
+
+import io.reactivex.Observable
+
+interface ApiHelper {
+
+    fun performServerLogin(request: LoginRequest.ServerLoginRequest): Observable<LoginResponse>
+
+    fun performFBLogin(request: LoginRequest.FacebookLoginRequest): Observable<LoginResponse>
+
+    fun performGoogleLogin(request: LoginRequest.GoogleLoginRequest): Observable<LoginResponse>
+
+    fun performLogoutApiCall(): Observable<LogoutResponse>
+
+}
