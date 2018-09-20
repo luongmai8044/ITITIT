e8b6f1138290aca829ccd931819c67602badd0ea
Luong Mai <mwd-luong.mai@mowede.com>
2018-09-20 15:43:29 +0700

    Intergrate Retrofit to the FW for calling API

--- app/src/main/java/mowede/framework/ititit/di/module/AppModule.kt
+++ app/src/main/java/mowede/framework/ititit/di/module/AppModule.kt
@@ -3,6 +3,9 @@ package mowede.framework.ititit.di.module
 import android.app.Application
 import android.arch.persistence.room.Room
 import android.content.Context
+import dagger.Module
+import dagger.Provides
+import io.reactivex.disposables.CompositeDisposable
 import mowede.framework.ititit.BuildConfig
 import mowede.framework.ititit.data.database.AppDatabase
 import mowede.framework.ititit.data.database.repository.options.OptionsRepo
@@ -10,17 +13,12 @@ import mowede.framework.ititit.data.database.repository.options.OptionsRepositor
 import mowede.framework.ititit.data.database.repository.questions.QuestionRepo
 import mowede.framework.ititit.data.database.repository.questions.QuestionRepository
 import mowede.framework.ititit.data.network.ApiHeader
-import mowede.framework.ititit.data.network.ApiHelper
-import mowede.framework.ititit.data.network.AppApiHelper
 import mowede.framework.ititit.data.preferences.AppPreferenceHelper
 import mowede.framework.ititit.data.preferences.PreferenceHelper
 import mowede.framework.ititit.di.ApiKeyInfo
 import mowede.framework.ititit.di.PreferenceInfo
 import mowede.framework.ititit.util.AppConstants
 import mowede.framework.ititit.util.SchedulerProvider
-import dagger.Module
-import dagger.Provides
-import io.reactivex.disposables.CompositeDisposable
 import javax.inject.Singleton
 
 @Module
@@ -53,10 +51,6 @@ class AppModule {
             : ApiHeader.ProtectedApiHeader = ApiHeader.ProtectedApiHeader(apiKey = apiKey,
             userId = preferenceHelper.getCurrentUserId(),
             accessToken = preferenceHelper.getAccessToken())
-
-    @Provides
-    @Singleton
-    internal fun provideApiHelper(appApiHelper: AppApiHelper): ApiHelper = appApiHelper
 
     @Provides
     @Singleton
